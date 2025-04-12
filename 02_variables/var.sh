3c4e7bf8915522b90978e5c904496970b75c14c7
shakyasimha <sushakya235@gmail.com>
2025-04-12 17:51:37 +0545

    added codes for variables

---
+++ 02_variables/var.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# 02_Variables/var.sh
+
+MY_MESSAGE="Hello World"
+MY_SHORT_MESSAGE=hi
+MY_NUMBER=1
+MY_PI=3.1415
+MY_OTHER_PI="3.142"
+MY_MIXED=123abc
+
+echo $MY_MESSAGE
+echo $MY_SHORT_MESSAGE
+echo $MY_NUMBER
+echo $MY_PI
+echo $MY_OTHER_PI
+echo $MY_MIXED
+
---
+++ 02_variables/var2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+echo "What is my name?"
+read MY_NAME
+echo "Hello $MY_NAME - hope you're doing well."
